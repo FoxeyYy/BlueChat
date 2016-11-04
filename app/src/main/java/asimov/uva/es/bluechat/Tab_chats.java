9a25fd810a862d12e7537765dca33195a6f1c286
FoxeyYy <hectorcampop@outlook.com>
2016-11-04 11:46:11 +0100

    Las tarjetas de contacto en chats ya se añaden por codigo

--- app/src/main/java/asimov/uva/es/bluechat/Tab_chats.java
+++ app/src/main/java/asimov/uva/es/bluechat/Tab_chats.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 /**
  * Created by Guti on 31/10/2016.
@@ -12,10 +13,22 @@ import android.view.ViewGroup;
 
 public class Tab_chats extends Fragment {
 
+    /**
+     * Lista donde añadir tarjetas de personas
+     */
+    private LinearLayout lista;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_chats, container, false);
+
+        lista = (LinearLayout) rootView.findViewById(R.id.lista_chats);
+
+        //TODO Stub
+        for(int i = 0; i < 4; i++)
+            inflater.inflate(R.layout.tarjeta_contacto, lista);
+
         return rootView;
     }
 }
