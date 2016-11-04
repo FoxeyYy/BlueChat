bbc546ce74b476bee298eb9aede0418d874d9023
FoxeyYy <hectorcampop@outlook.com>
2016-11-04 11:43:19 +0100

    Las tarjetas de contacto en descubrir ya se añaden por codigo

--- app/src/main/java/asimov/uva/es/bluechat/Tab_descubrir.java
+++ app/src/main/java/asimov/uva/es/bluechat/Tab_descubrir.java
@@ -4,19 +4,33 @@ package asimov.uva.es.bluechat;
  * Created by Guti on 31/10/2016.
  */
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class Tab_descubrir extends Fragment {
+
+    /**
+     * Lista donde añadir tarjetas de personas
+     */
+    private LinearLayout lista;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_descubrir, container, false);
 
+        lista = (LinearLayout) rootView.findViewById(R.id.lista_descubrir);
+
+        //TODO Stub
+        for(int i = 0; i < 9; i++)
+            inflater.inflate(R.layout.tarjeta_contacto, lista);
+
         return rootView;
     }
+
+
 }
